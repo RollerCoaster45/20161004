cc780cfcbdfedeb21581c0aa6452feaff0a8f06e
Ishaan <ishaanoberoi@gmail.com>
2016-10-06 21:55:51 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingCamera.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.lasarobotics.vision.opmode.VisionOpMode;
+
+/**
+ * Created by Ishaan Oberoi on 10/5/2016.
+ */
+@TeleOp(name="Test", group ="Open CV")
+public class TestingCamera extends VisionOpMode{
+
+}
+
